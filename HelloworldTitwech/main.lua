87290c3dd1377da0efc9145ebbbe3f3cbd892415
Titwech.wal <titwech.wal@ocsbstudent.ca>
2020-02-26 14:03:37 -0500

    Initial commit

---
+++ HelloworldTitwech/main.lua
@@ -0,0 +1,30 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+
+
+-- print greeting to the console
+print("**** Hello Titwech")
+
+-- hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- sets the background colour
+display.setDefault("background", 203/255, 255/255, 111/255)
+
+-- create a local variable
+local textObject
+
+-- displays text on the screen at position x = 500 and y = 500 with 
+-- a default font style and font size of 50
+textObject = display.newText( "Hello, Titwech!", 500, 300, nil, 120 )
+
+-- sets the color of the text
+textObject:setTextColor(64/255, 101/255, 0/255)
+
+-- displays text on the screen at position x = 500 and y = 500 with 
+-- a default font style and font size of 50
+textObject = display.newText( "By: Titwech.w", 500, 500, nil, 70 )
