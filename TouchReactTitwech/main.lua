4dc7601a20f441a52f7e12264b75ed59c488ac43
Titwech.wal <titwech.wal@ocsbstudent.ca>
2020-03-09 14:30:59 -0400

    added to assiment 4

--- TouchReactTitwech/main.lua
+++ TouchReactTitwech/main.lua
@@ -11,6 +11,10 @@ display.setDefault ("background", 198/255, 255/255, 207/255)
 -- hide stauts bar 
 display.setStatusBar(display.HiddenStatusBar)
 
+-- correct sound
+local correctSound = audio.loadSound("Sounds/Correct Answer Sound Effect.mp3") 
+local correctSoundChannel 
+
 -- create blue button, set it's position and make it visible
 local blueButton = display.newImageRect("Images/Fast Button Inactive@2x.png",198, 96)
 blueButton.x = display.contentWidth/2
@@ -31,11 +35,11 @@ textObject:setTextColor (10/255, 11/255, 159/255)
 textObject.isVisible = false
 
 -- create check mark, set it's position and make it visible
-local checkmark = display.newImageRect("Images/checkmark@2x.png",198, 96)
+local checkmark = display.newImageRect("Images/checkmark.png",198, 96)
 checkmark.x = display.contentWidth/2
-checkmark.y = display.contentHeight/2
+checkmark.y = display.contentHeight/4
 checkmark.isVisible = false
-
+ 
 
 -- Fuction: blueButtonListener
 -- Input: touch listener
@@ -46,12 +50,16 @@ local function blueButtonListener( touch )
 	if (touch.phase == "began") then
 		blueButton.isVisible = false
 		redButton.isVisible = true
+		checkmark.isVisible = true
 		textObject.isVisible = true
+		correctSoundChannel = audio.play(correctSound)
+
 	end
 
 	if (touch.phase == "ended") then 
 		blueButton.isVisible = false
 		redButton.isVisible = false
+		checkmark.isVisible = false
 		textObject.isVisible = false
 	end
 end
@@ -68,12 +76,14 @@ local function redButtonListener( touch )
 
 	if (touch.phase == "ended") then 
 		redButton.isVisible = false
+		checkmark.isVisible = false
 		blueButton.isVisible = true
 		textObject.isVisible = false
-		checkmark.isVisible = true
+	
 	end
 end
 
 -- add the respective listeners to each object
 redButton:addEventListener("touch", redButtonListener)
+
 
